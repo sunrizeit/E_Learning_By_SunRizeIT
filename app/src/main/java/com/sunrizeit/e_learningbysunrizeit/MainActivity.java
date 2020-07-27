3f9878a998498f4da81b23710aae2870fce70b47
sunrizeit <64303558+sunrizeit@users.noreply.github.com>
2020-07-27 20:55:03 +0530

    Initial commit

---
+++ app/src/main/java/com/sunrizeit/e_learningbysunrizeit/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sunrizeit.e_learningbysunrizeit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView videoList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoList=findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
---
+++ app/src/main/java/com/sunrizeit/e_learningbysunrizeit/VideoAdapter.java
@@ -0,0 +1,33 @@
+package com.sunrizeit.e_learningbysunrizeit;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.video_view,parent,false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
